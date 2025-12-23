1a80c2c91064a0015333ee65ca0b3c62639cbcbc
Faridekhalili <khalili.f@northeastern.edu>
2025-12-23 12:11:53 -0500

    Add augmented test.

---
+++ src/augmented.test.ts
@@ -0,0 +1,15 @@
+import { expect } from '@jest/globals';
+import { Point, GeoPoint, LatLng } from './geo-point';
+
+describe('Augmented tests', () => {
+    it('Sample 3', () => {
+        /**
+         * StringLiteral
+         * src/geo-point.ts:86:27
+         * -         throw new TypeError('GeoPoint: Argument must be an object');
+         * +         throw new TypeError("");
+         */
+        const o = <LatLng><unknown>'a'
+        expect(() => GeoPoint.fromObject(o)).toThrow('GeoPoint: Argument must be an object');
+    });
+});
