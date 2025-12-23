1a7ff527b099c7b42e5116d164030b96e07ad568
Faridekhalili <khalili.f@northeastern.edu>
2025-12-23 12:08:12 -0500

    Add augmented test.

---
+++ src/augmented.test.ts
@@ -0,0 +1,21 @@
+import { expect } from '@jest/globals';
+import { Point, GeoPoint, LatLng } from './geo-point';
+
+describe('Augmented tests', () => {
+
+    it('Sample 2', () => {
+        /**
+         * ArithmeticOperator
+         * src/geo-point.ts:155:15
+         * -       const y = sinθ * sinδ * cosφ1;
+         * +       const y = sinθ * sinδ / cosφ1;
+         */
+        expect(
+            GeoPoint.calculateDestination(
+                { latitude: 0.5, longitude: 0.5 },
+                30,
+                30
+            )
+        ).toEqual(new GeoPoint(0.5002336506056834, 0.5001349033824454))
+    });
+});
